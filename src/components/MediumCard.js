d1d51e88f8eb74cdf834a2e423279e68d56071d5
Amr Shakour <amrshakour97@gmail.com>
2022-02-28 14:49:18 -0500

    first commit

---
+++ src/components/MediumCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+function MediumCard({img,title}) {
+  return (
+    <div className='cursor-pointer hover:scale-105 transition transform duration-300 ease-out'>
+        {/* top */}
+        <div className='relative h-80 w-80 '>
+           <Image
+           src={img} layout="fill" className='rounded-xl'
+           />
+        </div>
+        {/* bottom */}
+        <div className='text-2xl mt-3'>
+            {title}
+        </div>
+    </div>
+  )
+}
+
+export default MediumCard
